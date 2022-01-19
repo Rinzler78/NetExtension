f63f24ca7231ba88c1920441db64aefa7269e7a3
Boris LECLERE <Borisleclere@gmail.com>
2022-01-19 16:31:30 +0100

    Add convert function

---
+++ src/Rinzler78.NetExtension.Standard/Types/ConvertHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Rinzler78.NetExtension.Types
+{
+    public static class ConvertHelper
+    {
+        public static T ConvertValue<T>(this object value)
+        {
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+    }
+}
