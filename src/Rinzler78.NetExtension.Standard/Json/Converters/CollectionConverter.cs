8702c2de6862facbde1c6e0f5d696b60bb99eac7
Boris LECLERE <Borisleclere@gmail.com>
2022-01-29 11:52:24 +0100

    Split in multiple files

---
+++ src/Rinzler78.NetExtension.Standard/Json/Converters/CollectionConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Rinzler78.NetExtension.Json.Converters
+{
+    public class CollectionConverter<CollectionElement> : JsonConverter<ICollection<CollectionElement>>
+    {
+        public override ICollection<CollectionElement> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return JsonSerializer.Deserialize<Collection<CollectionElement>>(ref reader, options);
+        }
+
+        public override void Write(Utf8JsonWriter writer, ICollection<CollectionElement> value, JsonSerializerOptions options)
+        {
+            JsonSerializer.Serialize(value, options);
+        }
+    }
+
+    public class ICollectionInterfaceConverterFactory<CollectionElement> : JsonConverterFactory
+    {
+        public readonly Type CollectionElementType = typeof(CollectionElement);
+
+        public override bool CanConvert(Type typeToConvert)
+        {
+            if (typeToConvert.Equals(typeof(ICollection<>).MakeGenericType(CollectionElementType))
+             && typeToConvert.GenericTypeArguments[0].Equals(CollectionElementType))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            return (JsonConverter)Activator.CreateInstance(
+                typeof(CollectionConverter<>).MakeGenericType(CollectionElementType));
+        }
+    }
+}
---
+++ src/Rinzler78.NetExtension.Standard/Json/Converters/InterfaceConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Rinzler78.NetExtension.Json.Converters
+{
+    public class InterfaceConverter<Implementation, Interface> : JsonConverter<Interface> where Implementation : class, Interface
+    {
+        public override Interface Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return JsonSerializer.Deserialize<Implementation>(ref reader, options);
+        }
+
+        public override void Write(Utf8JsonWriter writer, Interface value, JsonSerializerOptions options) { }
+    }
+
+    public class InterfaceConverterFactory<Interface, Implementation> : JsonConverterFactory
+        where Implementation : class, Interface, new()
+
+    {
+        public readonly Type InterfaceType = typeof(Interface);
+        public readonly Type ImplementationType = typeof(Implementation);
+
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return typeToConvert == InterfaceType;
+        }
+
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            var converterType = typeof(InterfaceConverter<,>).MakeGenericType(ImplementationType, InterfaceType);
+
+            return (JsonConverter)Activator.CreateInstance(converterType);
+        }
+    }
+}
