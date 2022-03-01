b1682bf5cca6e861827f47e74177724dd5c79cb1
Boris LECLERE <Borisleclere@gmail.com>
2022-03-01 16:01:22 +0100

    Add string comparison algo

--- src/Rinzler78.NetExtension.Standard/Strings/StringHelper.cs
+++ src/Rinzler78.NetExtension.Standard/Strings/StringHelper.cs
@@ -15,6 +15,52 @@ namespace Rinzler78.NetExtension.Strings
 {
     public static class StringHelper
     {
+        public static int ComputeLevenshteInDistance(this string source, string target)
+        {
+            if (string.IsNullOrEmpty(source))
+                return string.IsNullOrEmpty(target) ? 0 : target.Length;
+
+            if (string.IsNullOrEmpty(target))
+                return string.IsNullOrEmpty(source) ? 0 : source.Length;
+
+            int sourceLength = source.Length;
+            int targetLength = target.Length;
+
+            int[,] distance = new int[sourceLength + 1, targetLength + 1];
+
+            // Step 1
+            for (int i = 0; i <= sourceLength; distance[i, 0] = i++) ;
+            for (int j = 0; j <= targetLength; distance[0, j] = j++) ;
+
+            for (int i = 1; i <= sourceLength; i++)
+            {
+                for (int j = 1; j <= targetLength; j++)
+                {
+                    // Step 2
+                    int cost = (target[j - 1] == source[i - 1]) ? 0 : 1;
+
+                    // Step 3
+                    distance[i, j] = Math.Min(
+                                        Math.Min(distance[i - 1, j] + 1, distance[i, j - 1] + 1),
+                                        distance[i - 1, j - 1] + cost);
+                }
+            }
+
+            return distance[sourceLength, targetLength];
+        }
+
+        public static double CalculateSimilarity(this string source, string target)
+        {
+            if (string.IsNullOrEmpty(source))
+                return string.IsNullOrEmpty(target) ? 1 : 0;
+
+            if (string.IsNullOrEmpty(target))
+                return string.IsNullOrEmpty(source) ? 1 : 0;
+
+            double stepsToSame = ComputeLevenshteInDistance(source, target);
+            return (1.0 - (stepsToSame / (double)Math.Max(source.Length, target.Length)));
+        }
+
         public static bool ContainsAll(this string str, string[] words)
         {
             return words?.All((string arg) => str.Contains(arg)) ?? true;
