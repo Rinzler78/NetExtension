7a2500d25a95a27428611222e9e616de974bfe77
Boris LECLERE <Borisleclere@gmail.com>
2022-03-04 12:54:50 +0100

    Add csv string extension

---
+++ src/Rinzler78.NetExtension.Standard/Csv/CsvExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Rinzler78.NetExtension.Csv
+{
+    public static class CsvExtension
+    {
+        public static Task<IEnumerable<ReturnType>> LoadCSVAsync<ReturnType>(this string fileName, char separator = ';')
+            => Task.Run(() => fileName.LoadCSV<ReturnType>(separator));
+
+        public static IEnumerable<ReturnType> LoadCSV<ReturnType>(this string fileName, char separator = ';')
+        {
+            try
+            {
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    Delimiter = separator.ToString(),
+                };
+
+                var sourcePath = fileName;
+
+                using (var reader = new StreamReader(sourcePath))
+                using (var csv = new CsvReader(reader, config))
+                {
+                    return csv.GetRecords<ReturnType>().ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return default;
+        }
+    }
+}
