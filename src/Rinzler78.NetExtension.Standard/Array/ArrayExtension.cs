2a5070a8df365e452f383b693d813cd100605a16
Boris LECLERE <Borisleclere@gmail.com>
2022-03-15 15:46:20 +0100

    Add Array sum tool

---
+++ src/Rinzler78.NetExtension.Standard/Array/ArrayExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Rinzler78.NetExtension.Array
+{
+    public static class ArrayExtension
+    {
+        public static double[] SumArrays(params double[][] arrays)
+        {
+            int count = arrays.Max(arg => arg.Length);
+            var result = new double[count];
+
+            for(var i = 0; i < count; ++i)
+            {
+                double sum = default;
+
+                for (var y = 0; y < arrays.Length; ++y)
+                    sum += arrays[y][i];
+
+                result[i] = sum;
+            }
+
+            return result;
+        }
+    }
+}
