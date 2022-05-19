c834bef78bc807b125b073cfab5a3aba3837b7e2
Boris LECLERE <Borisleclere@gmail.com>
2022-05-19 13:07:09 +0200

    Add BaseRestApi

---
+++ src/Rinzler78.NetExtension.Standard/Rest/BaseRestApi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Rinzler78.NetExtension.Rest
+{
+    public abstract class BaseRestApi
+    {
+        public readonly Uri BaseUri;
+
+        protected BaseRestApi(string baseUri, string path) : this(new Uri(baseUri), path)
+        {
+
+        }
+
+        protected BaseRestApi(Uri baseUri, string path)
+        {
+            if (!baseUri.AbsoluteUri.EndsWith("/"))
+                baseUri = new Uri($"{baseUri.AbsoluteUri}/");
+
+            if (!path.EndsWith("/"))
+                path = $"{path}/";
+
+            BaseUri = new Uri(baseUri.AbsoluteUri + path);
+        }
+
+        protected string CreateUrlPath(string baseUrl, object[] args, string[] argsNames)
+        {
+            if (args != null && argsNames != null && args.Length == argsNames.Length)
+            {
+                if (args.Any(o => o != null))
+                {
+                    baseUrl += "?";
+
+                    for (var i = 0; i < args.Length; ++i)
+                    {
+                        var value = args[i];
+                        var name = argsNames[i];
+
+                        baseUrl += $"{name}={value}";
+
+                        if ((i + 1) < args.Length)
+                            baseUrl += "&";
+                    }
+                }
+            }
+
+            return baseUrl;
+        }
+    }
+}
