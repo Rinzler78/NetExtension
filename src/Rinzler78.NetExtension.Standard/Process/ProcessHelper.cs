6998c9cd977093a4972b9b11b01d00e39ba0a423
Boris LECLERE <Borisleclere@gmail.com>
2022-05-25 13:26:11 +0200

    Add process helpers

---
+++ src/Rinzler78.NetExtension.Standard/Process/ProcessHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Rinzler78.NetExtension.Process
+{
+
+    public static class ProcessHelper
+    {
+        public static ProcessOutputs WaitProcessOutputs(this System.Diagnostics.Process process)
+        {
+            process.WaitForExit();
+
+            return new(process.StandardOutput.ReadToEnd(), process.StandardError.ReadToEnd());
+        }
+
+        public static async Task<ProcessOutputs> WaitProcessOutputsAsync(this System.Diagnostics.Process process)
+        {
+            await process.WaitForExitAsync();
+
+            return new(process.StandardOutput.ReadToEnd(), process.StandardError.ReadToEnd());
+        }
+    }
+}
+
---
+++ src/Rinzler78.NetExtension.Standard/Process/ProcessOutputs.cs
@@ -0,0 +1,15 @@
+﻿namespace Rinzler78.NetExtension.Process
+{
+    public class ProcessOutputs
+    {
+        public string StdOut { get; }
+        public string StdErr { get; }
+
+        public ProcessOutputs(string stdOut, string stdErr)
+        {
+            StdOut = stdOut;
+            StdErr = stdErr;
+        }
+    }
+}
+
