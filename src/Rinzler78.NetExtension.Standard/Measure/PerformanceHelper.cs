89005d26be4cd6f50a0e13b50f7cab2a6f758c1b
Boris LECLERE <Borisleclere@gmail.com>
2022-10-11 19:00:39 +0200

    Allow to call func with argument

--- src/Rinzler78.NetExtension.Standard/Measure/PerformanceHelper.cs
+++ src/Rinzler78.NetExtension.Standard/Measure/PerformanceHelper.cs
@@ -27,9 +27,26 @@ namespace Rinzler78.NetExtension.Measure
             return result;
         }
 
+        public static ReturnType MeasureDuration<InType, ReturnType>(this Func<InType, ReturnType> func, InType input, Action<TimeSpan, ReturnType> duration = null)
+        {
+            var startDate = DateTime.UtcNow;
+
+            ReturnType result = func(input);
+
+            if (duration != null)
+                duration(DateTime.UtcNow - startDate, result);
+
+            return result;
+        }
+
         public static async Task<ReturnType> MeasureDurationAsync<ReturnType>(this Func<ReturnType> func, Action<TimeSpan, ReturnType> duration = null)
         {
             return await Task.Run(() => MeasureDuration(func, duration));
+        }
+
+        public static async Task<ReturnType> MeasureDurationAsync<InType, ReturnType>(this Func<InType, ReturnType> func, InType input, Action<TimeSpan, ReturnType> duration = null)
+        {
+            return await Task.Run(() => MeasureDuration(func, input, duration));
         }
     }
 }
