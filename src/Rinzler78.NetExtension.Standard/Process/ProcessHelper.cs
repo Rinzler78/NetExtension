3494c6c8bf376a3834772bf768189b84c9debfed
Boris LECLERE <Borisleclere@gmail.com>
2022-10-12 15:08:56 +0200

    Update packages Try to read stdout and strerr at the same time

--- src/Rinzler78.NetExtension.Standard/Process/ProcessHelper.cs
+++ src/Rinzler78.NetExtension.Standard/Process/ProcessHelper.cs
@@ -1,11 +1,16 @@
-﻿namespace Rinzler78.NetExtension.Process;
+﻿using System.Threading.Tasks;
+using Rinzler78.NetExtension.Tasks;
+
+namespace Rinzler78.NetExtension.Process;
 
 public static class ProcessHelper
 {
     public static ProcessOutputs WaitProcessOutputs(this System.Diagnostics.Process process)
     {
-        var stdOut = process.StandardOutput.ReadToEnd();
-        var stdErr = process.StandardError.ReadToEnd();
+        var stds = Task.WhenAll(process.StandardOutput.ReadToEndAsync(), process.StandardError.ReadToEndAsync()).Result;
+
+        var stdOut = stds[0];
+        var stdErr = stds[1];
 
         //process.WaitForExit();
 
