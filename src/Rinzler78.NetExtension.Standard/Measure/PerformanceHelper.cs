b100c31502b60ce2fb3fbfaf6f23410a11731b24
Boris LECLERE <Borisleclere@gmail.com>
2022-10-25 16:43:12 +0200

    Add out variantes

--- src/Rinzler78.NetExtension.Standard/Measure/PerformanceHelper.cs
+++ src/Rinzler78.NetExtension.Standard/Measure/PerformanceHelper.cs
@@ -5,48 +5,72 @@ namespace Rinzler78.NetExtension.Measure
 {
     public static class PerformanceHelper
     {
-        public static void MeasureDuration(this Action action, Action<TimeSpan> duration = null)
+        public static void MeasureDuration(this Action action, out TimeSpan duration)
         {
             var startDate = DateTime.UtcNow;
 
             action();
 
-            if (duration != null)
-                duration(DateTime.UtcNow - startDate);
+            duration = DateTime.UtcNow - startDate;
         }
 
-        public static ReturnType MeasureDuration<ReturnType>(this Func<ReturnType> func, Action<TimeSpan, ReturnType> duration = null)
+        public static void MeasureDuration(this Action action, Action<TimeSpan> durationAction = null)
+        {
+            action.MeasureDuration(out var duration);
+
+            durationAction?.Invoke(duration);
+        }
+
+        public static ReturnType MeasureDuration<ReturnType>(this Func<ReturnType> func, out TimeSpan duration)
         {
             var startDate = DateTime.UtcNow;
 
             ReturnType result = func();
 
-            if (duration != null)
-                duration(DateTime.UtcNow - startDate, result);
+            duration = DateTime.UtcNow - startDate;
 
             return result;
         }
 
-        public static ReturnType MeasureDuration<InType, ReturnType>(this Func<InType, ReturnType> func, InType input, Action<TimeSpan, ReturnType> duration = null)
+        public static ReturnType MeasureDuration<ReturnType>(this Func<ReturnType> func, Action<TimeSpan, ReturnType> durationAction = null)
+        {
+            ReturnType result = func.MeasureDuration(out var duration);
+
+            durationAction?.Invoke(duration, result);
+
+            return result;
+        }
+
+        public static ReturnType MeasureDuration<InType, ReturnType>(this Func<InType, ReturnType> func, InType input, out TimeSpan duration)
         {
             var startDate = DateTime.UtcNow;
 
             ReturnType result = func(input);
 
-            if (duration != null)
-                duration(DateTime.UtcNow - startDate, result);
+            duration = DateTime.UtcNow - startDate;
 
             return result;
         }
 
-        public static async Task<ReturnType> MeasureDurationAsync<ReturnType>(this Func<ReturnType> func, Action<TimeSpan, ReturnType> duration = null)
+        public static ReturnType MeasureDuration<InType, ReturnType>(this Func<InType, ReturnType> func, InType input, Action<TimeSpan, ReturnType> durationAction = null)
         {
-            return await Task.Run(() => MeasureDuration(func, duration));
+            var startDate = DateTime.UtcNow;
+
+            ReturnType result = func(input);
+
+            durationAction?.Invoke(DateTime.UtcNow - startDate, result);
+
+            return result;
         }
 
-        public static async Task<ReturnType> MeasureDurationAsync<InType, ReturnType>(this Func<InType, ReturnType> func, InType input, Action<TimeSpan, ReturnType> duration = null)
+        public static async Task<ReturnType> MeasureDurationAsync<ReturnType>(this Func<ReturnType> func, Action<TimeSpan, ReturnType> durationAction = null)
         {
-            return await Task.Run(() => MeasureDuration(func, input, duration));
+            return await Task.Run(() => MeasureDuration(func, durationAction));
+        }
+
+        public static async Task<ReturnType> MeasureDurationAsync<InType, ReturnType>(this Func<InType, ReturnType> func, InType input, Action<TimeSpan, ReturnType> durationAction = null)
+        {
+            return await Task.Run(() => MeasureDuration(func, input, durationAction));
         }
     }
 }
