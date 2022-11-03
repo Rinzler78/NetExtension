210f7c24f1b96b5629774a422f5b17107c900072
Boris LECLERE <Borisleclere@gmail.com>
2022-11-03 16:58:54 +0100

    Add Enum parser helper

--- src/Rinzler78.NetExtension.Standard/Enums/EnumsHelper.cs
+++ src/Rinzler78.NetExtension.Standard/Enums/EnumsHelper.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using Newtonsoft.Json.Linq;
 
 namespace Rinzler78.NetExtension.Enums;
 
@@ -32,6 +38,41 @@ public static class EnumsHelper
         }
         catch (Exception ex)
         {
+        }
+
+        return default;
+    }
+
+    public static Enumtype Parse<Enumtype>(this string str)
+        where Enumtype : Enum
+    {
+        return EnumParser<Enumtype>.Parse(str);
+    }
+}
+
+public static class EnumParser<Enumtype>
+    where Enumtype : Enum
+{
+    public static readonly ReadOnlyDictionary<string, Enumtype> EnumValuesDictionary = new(Enum
+            .GetValues(typeof(Enumtype))
+            .Cast<Enumtype>()
+            .SelectMany(v =>
+                new[] {
+                                (n: v.ToString().ToLower(), v),
+                                (n: ((int)(object)v).ToString(), v)
+            })
+            .ToDictionary(i => i.n, i => i.v, StringComparer.OrdinalIgnoreCase));
+
+    public static Enumtype Parse(string str)
+    {
+        try
+        {
+            return EnumValuesDictionary[str];
+            //return EnumValuesDictionary.AsParallel().First(kvp => string.Equals(kvp.Key, str, StringComparison.InvariantCultureIgnoreCase)).Value;
+        }
+        catch
+        {
+            Console.WriteLine($"{typeof(Enumtype).Name} cannot be parse using {str}");
         }
 
         return default;
