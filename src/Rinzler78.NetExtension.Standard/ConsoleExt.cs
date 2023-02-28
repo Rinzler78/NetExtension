1317a4ea37758cf187a6e0185bb3e852fd6cd0e5
Boris LECLERE <Borisleclere@gmail.com>
2023-02-28 22:03:24 +0100

    Add Console and Uri helper

---
+++ src/Rinzler78.NetExtension.Standard/ConsoleExt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Rinzler78.NetExtension;
+
+public static class ConsoleExt
+{
+    public static readonly object Locker = new();
+
+    public static void ConsoleWriteLine(this string str, ConsoleColor? backgroundColor = null, ConsoleColor? foregroundColor = null)
+    {
+        if (backgroundColor != null || foregroundColor != null)
+        {
+            lock (Locker)
+            {
+                var previousForegroundColor = Console.ForegroundColor;
+                var previousBackgroundColor = Console.BackgroundColor;
+
+                Console.ForegroundColor = foregroundColor ?? previousForegroundColor;
+                Console.BackgroundColor = backgroundColor ?? previousBackgroundColor;
+
+                Console.WriteLine(str);
+
+                Console.ForegroundColor = previousForegroundColor;
+                Console.BackgroundColor = previousBackgroundColor;
+            }
+        }
+        else
+            Console.WriteLine(str);
+    }
+}
+
---
+++ src/Rinzler78.NetExtension.Standard/UriHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Rinzler78.NetExtension;
+
+public static class UriHelper
+{
+    public static string ToFullEnpoint(this Uri uri)
+        => $"{uri.Scheme}://{uri.Host}:{uri.Port}/";
+}
+
